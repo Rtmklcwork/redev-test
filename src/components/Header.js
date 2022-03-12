38c01a2bc1a9f05e56ceaf92412dca67a0b06392
Artem Kalach <rtmklcwork@gmai.com>
2022-03-12 11:22:02 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='olala.jpg'/>
+        </header>
+    )
+}
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://media.istockphoto.com/photos/mountain-landscape-picture-id517188688?s=612x612'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    <div>
+                        Post1
+                    </div>
+                    <div>
+                        Post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
